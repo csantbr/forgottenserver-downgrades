73c6e72610596bf574d73169e875e9e4601c5780
Felipe Muniz <felipe.silva@virtus.ufcg.edu.br>
2024-07-16 22:45:44 -0300

    fix: adjustments on some packets sended to client

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -2,10 +2,6 @@ function Monster:onDropLoot(corpse)
 	if hasEvent.onDropLoot then
 		Event.onDropLoot(self, corpse)
 	end
-	local player = Player(corpse:getCorpseOwner())
-	if player then
-		player:updateKillTracker(self, corpse)
-	end
 end
 
 function Monster:onSpawn(position, startup, artificial)
